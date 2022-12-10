6950059b49a08da7d4172cf7f8bf3f09f70e8b3b
atique-atq <atique.atq@gmail.com>
2022-12-10 13:08:43 +0600

    added navbar

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Shared/Navbar/Navbar';
 
 const Main = () => {
     return (
         <div>
-            this is layout.
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        portfoliotheme: {
+          "base-100": "#ECF0F3",
+        }
+      }
+    ]
+  },
   theme: {
     extend: {},
   },
