7220a487fc9aca196257605995402e0e0d97256b
atique-atq <atique.atq@gmail.com>
2023-01-02 23:34:17 +0600

    added copyright in footer

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import AboutMe from '../AboutMe/AboutMe';
-import Banner from '../Banner/Banner';
-import Contact from '../Contact/Contact';
-import Projects from '../Projects/Projects';
-import Skills from '../Skills/Skills';
+import React from "react";
+import AboutMe from "../AboutMe/AboutMe";
+import Banner from "../Banner/Banner";
+import Contact from "../Contact/Contact";
+import Projects from "../Projects/Projects";
+import Footer from "../Shared/Footer/Footer";
+import Skills from "../Skills/Skills";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-            <Projects></Projects>
-            <Skills></Skills>
-            <AboutMe></AboutMe>
-            <Contact></Contact>
-        </div>
-    );
+  return (
+    <div>
+      <Banner></Banner>
+      <Projects></Projects>
+      <Skills></Skills>
+      <AboutMe></AboutMe>
+      <Contact></Contact>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Home;
