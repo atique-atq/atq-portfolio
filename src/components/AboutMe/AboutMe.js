f26630370988d01d8dd0a650f8c2f4a04b7255f7
atique-atq <atique.atq@gmail.com>
2023-01-04 23:08:40 +0600

    added outline

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -85,12 +85,12 @@ const AboutMe = () => {
             </div>
           </div>
 
-          <div className="bg-base-100 p-3 lg:w-7/12 w-11/12 shadow-lg rounded-md">
+          <div className="bg-base-100 p-3 lg:w-8/12 w-11/12 shadow-lg rounded-md">
             <p className="py-3 font-semibold font-mono text-shadow-lg">
               Professional Experiences:
             </p>
-            <div className="outline outline-1 rounded-lg p-2 text-xs shadow-lg shadow-white">
-              <div className="flex flex-wrap justify-between font-semibold">
+            <div className="outline outline-slate-300 outline-1 rounded-lg p-2 text-xs shadow-lg shadow-white">
+              <div className="flex flex-nowrap justify-between font-semibold">
                 <div>1. Assistant Programmer</div>
 
                 <div className="text-slate-500">
@@ -110,10 +110,10 @@ const AboutMe = () => {
               <div className="flex justify-end">
                 <label
                   htmlFor="my-modal-6"
-                  className="outline outline-0 p-1 mt-2 rounded content-end bg-[#d2d5d6] cursor-pointer
+                  className="outline outline-0 p-1 mt-0 rounded content-end bg-[#d2d5d6] cursor-pointer
                   hover:bg-white"
                 >
-                  Show Details
+                  Details
                 </label>
                 {/* Put this part before </body> tag */}
                 <input
@@ -140,30 +140,57 @@ const AboutMe = () => {
               </div>
             </div>
 
-            <p className="pt-2">
-              <small>
-                <strong> 2. Software Engineer - Welldev Bangladesh Ltd</strong>{" "}
-                <span className="italic"> [2017 Nov – 2019 Jun]</span>
-                <br />
-                Technologies: Rails, Rspec, PostgreSQL, Hotel, Car, Flight
-                Booking API (GDS)
-              </small>
-            </p>
+            <div className="outline outline-1 rounded-lg p-1 mt-2 text-xs shadow-lg shadow-white">
+              <div className="flex flex-nowrap justify-between font-semibold">
+                <div>1. Assistant Programmer</div>
 
-            <p className="pt-2">
-              <small>
-                {" "}
-                <strong>
-                  {" "}
-                  3. Software Engineering Associate - Accenture Bangladesh Ltd
-                </strong>{" "}
-                <span className="italic"> [2017 Nov – 2019 Jun] </span>
-                <br />
-                Technologies: Oracle, JavaScript, HTML, CSS, Java
-              </small>
-            </p>
+                <div className="text-slate-500">
+                  <span className="text-slate-400 italic">@</span> DoICT, ICT
+                  Division
+                </div>
+              </div>
+              <div className="mt-2">
+                <p>
+                  <span className="font-semibold">Technologies: </span>{" "}
+                  Bootstrap, Tailwind CSS, Javascript, React.js, Node.js
+                </p>
+              </div>
+
+              {/* modal */}
+              {/* The button to open modal */}
+              <div className="flex justify-end">
+                <label
+                  htmlFor="my-modal-6"
+                  className="outline outline-0 p-1 mt-0 rounded content-end bg-[#d2d5d6] cursor-pointer
+                  hover:bg-white"
+                >
+                  Details
+                </label>
+                {/* Put this part before </body> tag */}
+                <input
+                  type="checkbox"
+                  id="my-modal-6"
+                  className="modal-toggle"
+                />
+                <div className="modal modal-bottom sm:modal-middle">
+                  <div className="modal-box">
+                    <h3 className="font-bold text-lg">
+                      Congratulations random Internet user!
+                    </h3>
+                    <p className="py-4">
+                      You've been selected for a chance to get one year of
+                      subscription to use Wikipedia for free!
+                    </p>
+                    <div className="modal-action">
+                      <label htmlFor="my-modal-6" className="btn">
+                        Yay!
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-
           <div
             className="bg-[#D0DAE2] rounded-lg p-1 shadow-lg lg:w-7/12 w-11/12
                     transition ease-in-out delay- hover:-translate-y-1 hover:scale-100 hover:bg-white duration-900 hover:rounded-md hover:shadow-xl hover:shadow-white"
